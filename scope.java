fc5a1d93dda09147a23c066f74a04dc5d7d5de34
karanagarwal17 <karanagarwal1702@gmail.com>
2017-01-11 10:47:01 +0530

    learned about the java scopes

---
+++ scope.java
@@ -0,0 +1,11 @@
+class scope {
+  public static void main (String args[]){
+    int x = 10;
+    if( x == 10 ){
+      int y = 20;
+      System.out.println("x and y are " + x + " " + y);
+    }
+    //System.out.println("x and y are" + x + " " + y); *This won't work *
+    System.out.println("x and y are " + x + " ");
+  }
+}
