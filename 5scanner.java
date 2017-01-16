be9a8dbd8ab90fd4733d56d322aa4333c9476254
karanagarwal17 <karanagarwal1702@gmail.com>
2017-01-16 11:48:55 +0530

    learnt to take inputs

---
+++ 5scanner.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class scanner {
+  public static void main (String args[]){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter String");
+    String s = scanner.nextLine();
+    System.out.println("String is " + s);
+  }
+}
